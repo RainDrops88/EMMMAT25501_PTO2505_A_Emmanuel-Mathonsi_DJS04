389f4ba433fae77b866139b388d61f9160968973
Emmanuel <07942jerry@gmail.com>
2026-03-09 19:54:20 +0200

    removing button and adding props for search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
-export default function Header(search) {
+export default function Header({ search, onSearchChange }) {
 
   return (
     <header className="app-header">
       <h1>🎙️ Podcast App</h1>
       <div className="search">
-      <input className="search-bar" type="text" placeholder="" onChange={search} />
-      <button className="search-btn">Search</button>
+      <input className="search-bar" type="text" placeholder="Search podcasts..." value={search} onChange={onSearchChange} />
+      
       </div>
     </header>
 
