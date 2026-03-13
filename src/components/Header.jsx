42c065df15d5ddebd7129d8f76a96deb99f4c471
Emmanuel <07942jerry@gmail.com>
2026-03-13 21:06:28 +0200

    importing usecontext

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
-export default function Header({ search, onSearchChange }) {
+import { useContext } from "react";
+import { PodcastContext } from "../context/PodcastContext";
+
+export default function Header() {
+  const { search, setSearch } = useContext(PodcastContext);
 
   return (
     <header className="app-header">
       <h1>🎙️ Podcast App</h1>
       <div className="search">
-      <input className="search-bar" type="text" placeholder="Search podcasts..." value={search} onChange={onSearchChange} />
+      <input className="search-bar" type="text" placeholder="Search podcasts..." value={search} onChange={(e) => setSearch(e.target.value)} />
       
       </div>
     </header>
